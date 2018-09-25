b0c21f924246ce7c2c9ae80427a413831fc502de
petrix <yuri.petrik@gmail.com>
2018-09-26 00:41:16 +0300

    xxx

--- src/hw-12-2/hw-12-2.js
+++ src/hw-12-2/hw-12-2.js
@@ -9,29 +9,5 @@ $(".row-title").click(function () {
     
     $('.row-title').not($(this)).removeClass('row-active');
     $(this).addClass('row-active');
-
-    // $(this).next().toggleClass('active');
-    // $(this).css('border-radius', '10px 10px 0 0');
-    
-    console.log(0.1 + 0.3);
 });
-// $(".row-title").click(function () {
-//     $(".row-content").not($(this).next()).removeClass('visible');
-//     $(this).next().toggleClass('visible');
-// });
-
-
-// const rows = document.querySelectorAll('.rows');
-// var i = 0;
-// for (i; i < rows.length ; i++){
-//     rows[i].onclick = show;
-//     console.log(rows[i]);
-//     // console.log(this);
-// }
-// function show(){
-//     // var x = div.querySelectorAll('.row-title');
-//     console.log(this.value);
-    // console.log(x);
-    // console.log(i);
-// }
 
--- src/hw-12-j/hw-12-j.js
+++ src/hw-12-j/hw-12-j.js
@@ -1,42 +1,54 @@
 import './hw-12-j.scss';
 
 
-const tabs = document.querySelectorAll('.tabs_selector');
-const tabcontent1 = document.querySelector('.tabcontent1');
-const tabcontent2 = document.querySelector('.tabcontent2');
-const tabcontent3 = document.querySelector('.tabcontent3');
-const selecttab1 = document.querySelector('.selecttab1');
-const selecttab2 = document.querySelector('.selecttab2');
-const selecttab3 = document.querySelector('.selecttab3');
-var i = 0;
-for (i; i < tabs.length; i++) {
-    tabs[i].onclick = selectTab;
-}
+// const tabs = document.querySelectorAll('.tabs_selector');
+// const tabcontent1 = document.querySelector('.tabcontent1');
+// const tabcontent2 = document.querySelector('.tabcontent2');
+// const tabcontent3 = document.querySelector('.tabcontent3');
+// const selecttab1 = document.querySelector('.selecttab1');
+// const selecttab2 = document.querySelector('.selecttab2');
+// const selecttab3 = document.querySelector('.selecttab3');
+// var i = 0;
+// for (i; i < tabs.length; i++) {
+//     tabs[i].onclick = selectTab;
+// }
 
-function selectTab() {
-    // console.log(this);
-    if (this.classList.contains('selecttab1')) {
-        selecttab1.classList.add('selected-tab');
-        selecttab2.classList.remove('selected-tab');
-        selecttab3.classList.remove('selected-tab');
-        tabcontent1.classList.add('visible');
-        tabcontent2.classList.remove('visible');
-        tabcontent3.classList.remove('visible');
-    } else if (this.classList.contains('selecttab2')) {
-        selecttab1.classList.remove('selected-tab');
-        selecttab2.classList.add('selected-tab');
-        selecttab3.classList.remove('selected-tab');
-        tabcontent1.classList.remove('visible');
-        tabcontent2.classList.add('visible');
-        tabcontent3.classList.remove('visible');
-    } else
-    // if (this.classList.contains('selecttab3')) 
-    {
-        selecttab1.classList.remove('selected-tab');
-        selecttab2.classList.remove('selected-tab');
-        selecttab3.classList.add('selected-tab');
-        tabcontent1.classList.remove('visible');
-        tabcontent2.classList.remove('visible');
-        tabcontent3.classList.add('visible');
-    }
-}
+// function selectTab() {
+//     // console.log(this);
+//     if (this.classList.contains('selecttab1')) {
+//         selecttab1.classList.add('selected-tab');
+//         selecttab2.classList.remove('selected-tab');
+//         selecttab3.classList.remove('selected-tab');
+//         tabcontent1.classList.add('visible');
+//         tabcontent2.classList.remove('visible');
+//         tabcontent3.classList.remove('visible');
+//     } else if (this.classList.contains('selecttab2')) {
+//         selecttab1.classList.remove('selected-tab');
+//         selecttab2.classList.add('selected-tab');
+//         selecttab3.classList.remove('selected-tab');
+//         tabcontent1.classList.remove('visible');
+//         tabcontent2.classList.add('visible');
+//         tabcontent3.classList.remove('visible');
+//     } else
+//     // if (this.classList.contains('selecttab3')) 
+//     {
+//         selecttab1.classList.remove('selected-tab');
+//         selecttab2.classList.remove('selected-tab');
+//         selecttab3.classList.add('selected-tab');
+//         tabcontent1.classList.remove('visible');
+//         tabcontent2.classList.remove('visible');
+//         tabcontent3.classList.add('visible');
+//     }
+// }
+$(".selecttab1").click(function(){
+    $(".tabcontent1").addClass("visible");
+    $(".tabcontent").not($(".tabcontent1")).removeClass("visible");
+})
+$(".selecttab2").click(function(){
+    $(".tabcontent2").addClass("visible");
+    $(".tabcontent").not($(".tabcontent2")).removeClass("visible");
+})
+$(".selecttab3").click(function(){
+    $(".tabcontent3").addClass("visible");
+    $(".tabcontent").not($(".tabcontent3")).removeClass("visible");
+})
