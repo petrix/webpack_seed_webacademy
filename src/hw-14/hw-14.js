d68250a5128a53e9df563f2fc3ffec0a398f2d5e
petrix <yuri.petrik@gmail.com>
2018-09-29 20:03:46 +0300

    xxx

--- src/hw-14/hw-14.js
+++ src/hw-14/hw-14.js
@@ -4,7 +4,8 @@ import './hw-14.scss';
 /////нарисовать login окно
 
 $('button').click(function(){
-    $(this).parent().children('p').toggleClass('active');
+    // $(this).parent().children('p').toggleClass('active');
+    $(this).prev('p').toggleClass('active');
 });
 $('.on-all').click(function(){
     $(this).parent().parent().children().find('p').addClass('active');
