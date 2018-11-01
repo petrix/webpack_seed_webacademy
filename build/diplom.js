12b1a193ccee0a7451744a8e603e7f4226ca0899
petrix <yuri.petrik@gmail.com>
2018-11-01 04:28:55 +0200

    xxx

--- build/diplom.js
+++ build/diplom.js
@@ -86,6 +86,32 @@ $(document).ready(function () {
     $.get("https://ipinfo.io", function (response) {
         console.log(response.ip, response.country, response);
     }, "jsonp");
+    var selectorPosition = -200;
+    var activeProp = 2;
+    $('.carselector-next').click(function () {
+        if (activeProp < 8) {
+            $('.carselector-timeline').find('.active').removeClass('active').next().addClass('active');
+            selectorPosition -= 337;
+            $('.carselector-timeline').css('transform', 'translateX(' + selectorPosition + 'px)');
+            activeProp += 1;
+            $('.carselector-prev').removeClass('disabled');
+            if (activeProp == 8) {
+                $('.carselector-next').addClass('disabled');
+            }
+        }
+    });
+    $('.carselector-prev').click(function () {
+        if (activeProp > 1) {
+            $('.carselector-timeline').find('.active').removeClass('active').prev().addClass('active');
+            selectorPosition += 337;
+            $('.carselector-timeline').css('transform', 'translateX(' + selectorPosition + 'px)');
+            activeProp -= 1;
+            $('.carselector-next').removeClass('disabled');
+            if (activeProp == 1) {
+                $('.carselector-prev').addClass('disabled');
+            }
+        }
+    });
 });
 
 /***/ }),
--- src/diplom/diplom.js
+++ src/diplom/diplom.js
@@ -1,11 +1,34 @@
 import './diplom.scss';
-$(document).ready(function(){
-    
+$(document).ready(function () {
 
-    $.get("https://ipinfo.io", function(response) {
+    $.get("https://ipinfo.io", function (response) {
         console.log(response.ip, response.country, response);
-      }, "jsonp")
-
-
+    }, "jsonp");
+    var selectorPosition = -200;
+    var activeProp = 2;
+    $('.carselector-next').click(function () {
+        if (activeProp < 8) {
+            $('.carselector-timeline').find('.active').removeClass('active').next().addClass('active');
+            selectorPosition -= 337;
+            $('.carselector-timeline').css('transform', 'translateX(' + selectorPosition + 'px)');
+            activeProp += 1;
+            $('.carselector-prev').removeClass('disabled');
+            if (activeProp == 8) {
+                $('.carselector-next').addClass('disabled');
+            }
+        }
+    });
+    $('.carselector-prev').click(function () {
+        if (activeProp > 1) {
+            $('.carselector-timeline').find('.active').removeClass('active').prev().addClass('active');
+            selectorPosition += 337;
+            $('.carselector-timeline').css('transform', 'translateX(' + selectorPosition + 'px)');
+            activeProp -= 1;
+            $('.carselector-next').removeClass('disabled');
+            if (activeProp == 1) {
+                $('.carselector-prev').addClass('disabled');
+            }
+        }
+    });
 
 });
