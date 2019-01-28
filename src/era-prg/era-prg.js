33b24be76bf252d2e7118d5142ff9049c8576b4b
petrix <yuri.petrik@gmail.com>
2019-01-28 08:37:26 +0200

    zzzzz

--- src/era-prg/era-prg.js
+++ src/era-prg/era-prg.js
@@ -1,16 +1,1 @@
 import './era-prg.scss';
-
-
-
-
-var int;
-
-$('.circle').on('click', function () {
-    clearInterval(int);
-    int = setInterval(function () {
-        var date = new Date();
-        $('.circle').toggleClass('active');
-        console.log($('.circle').position(), date);
-    }, 1000);
-
-});
